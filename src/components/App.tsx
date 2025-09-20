23fa82c6c7483aef8c36412f1612303978526ed9
Kirill Lapushinskiy <kirill@lapushinskiy.ru>
2025-09-20 12:01:27 +0300

    FEAT: add MainPage & PlaceCard

---
+++ src/components/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MainPage from './main-page';
+
+const App: React.FC = () => <MainPage />;
+
+export default App;
---
+++ src/components/place-card/PlaceCard.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+interface PlaceCardProps {
+  // id: number;
+  title: string;
+  type: string;
+  price: number;
+  rating: number;
+  image: string;
+  isPremium?: boolean;
+  isFavorite?: boolean;
+}
+
+const PlaceCard: React.FC<PlaceCardProps> = ({
+  title,
+  type,
+  price,
+  rating,
+  image,
+  isPremium = false,
+  isFavorite = false,
+}) => (
+  <article className="cities__card place-card">
+    {isPremium && (
+      <div className="place-card__mark">
+        <span>Premium</span>
+      </div>
+    )}
+    <div className="cities__image-wrapper place-card__image-wrapper">
+      <a href="#">
+        <img className="place-card__image" src={image} width="260" height="200" alt="Place image" />
+      </a>
+    </div>
+    <div className="place-card__info">
+      <div className="place-card__price-wrapper">
+        <div className="place-card__price">
+          <b className="place-card__price-value">&euro;{price}</b>
+          <span className="place-card__price-text">&#47;&nbsp;night</span>
+        </div>
+        <button
+          className={`place-card__bookmark-button ${isFavorite ? 'place-card__bookmark-button--active' : ''} button`}
+          type="button"
+        >
+          <svg className="place-card__bookmark-icon" width="18" height="19">
+            <use xlinkHref="#icon-bookmark"></use>
+          </svg>
+          <span className="visually-hidden">{isFavorite ? 'In bookmarks' : 'To bookmarks'}</span>
+        </button>
+      </div>
+      <div className="place-card__rating rating">
+        <div className="place-card__stars rating__stars">
+          <span style={{width: `${rating * 20}%`}}></span>
+          <span className="visually-hidden">Rating</span>
+        </div>
+      </div>
+      <h2 className="place-card__name">
+        <a href="#">{title}</a>
+      </h2>
+      <p className="place-card__type">{type}</p>
+    </div>
+  </article>
+);
+
+export default PlaceCard;
